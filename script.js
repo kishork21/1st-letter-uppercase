3eb282fc15ff6d6316c7c221010cfcb0617ad040
Kishor <mewadakishor2002@gmail.com>
2022-10-08 23:49:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function upperCase() {
+    // get the value of input field
+    var value1 = document.getElementById('inputField').value;
+
+    // convert in the uppercase
+    var convertValue = value1.toUpperCase(0,value1.length);
+
+    // print the value
+    document.getElementById('showData-1').innerHTML = convertValue;
+
+    if(value1==""){
+        confirm("enter the text")
+    }
+    
+
+}
+
+function lowerCase() {
+    // get the value of input field
+    var value2 = document.getElementById('inputField').value;
+
+    // convert in the uppercase
+    var convertValue = value2.toLowerCase(0,value2.length);
+
+    // print the value
+    document.getElementById('showData-2').innerHTML = convertValue;
+
+    if(value2==""){
+        confirm("enter the text")
+    }
+
+}
